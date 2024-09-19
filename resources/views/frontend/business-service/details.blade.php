2c4c51d2ce623e1ff56f8c0d25fdf3712b7c4369
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-09-19 17:59:57 +0600

    Fixed issue

--- resources/views/frontend/business-service/details.blade.php
+++ resources/views/frontend/business-service/details.blade.php
@@ -2,20 +2,23 @@
 
 @section('content')
 
-<section class="mb-5">
-    <div class="container mb-5">
-        <div class="row justify-content-center">
-
-                <div class="content mt-4">
-                    <h3 class="txt-primary poppins-medium display-6">{{ $businessService->short_title }}</h3>
-                    <hr class="my-4">
-                    <p class="txt-primary lead" style="text-align: justify;">
-                        {!! $businessService->long_description !!}
+<section class="py-5 bg-primary">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-12">
+                <div class="text-center w-100  mx-auto">
+                    <div class=" px-5">
+                        <h1 class="text-light poppins-bold">{{ $businessService->long_title }}</h1>
+                    </div>
+                    <p class="text-light w-75 text-center my-3 mx-auto">
+                        {!! $businessService->short_description !!}
                     </p>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+{!! $businessService->long_description !!}
 
 @endsection
