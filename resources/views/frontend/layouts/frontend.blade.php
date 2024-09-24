d1cc9b779bf3d0da5c9084106ff926300d92a1f6
shakil <kazimuhammadullah@gmail.com>
2024-09-24 22:28:37 +0600

    calender css add

--- resources/views/frontend/layouts/frontend.blade.php
+++ resources/views/frontend/layouts/frontend.blade.php
@@ -40,30 +40,33 @@
         <style>
             #datepicker .table-condensed{
                 width:100%;
-                }
+            }
                 
-                .datepicker th.dow{
+            .datepicker th.dow{
                 font-weight: 100;
                 color: #1A3A66;
                 text-transform: uppercase;
-                }
+            }
                 
-                .datepicker table tr td.old {
+            .datepicker table tr td.old {
                 color: #8095C5;
-                }
+            }
                 
-                .datepicker table tr {
+            .datepicker table tr {
                 line-height:3;
-                }
+            }
                 
-                .datepicker table tr td.active{
+            .datepicker table tr td.active{
                 background-color: #1a3a66;
                 background-image:unset;
-                }
+            }
     
-                .datepicker{
+            .datepicker{
                 margin-bottom:25px;
-                }
+            }
+            .datepicker-inline {
+                width: unset;
+            }
         </style>
 
     </head>
