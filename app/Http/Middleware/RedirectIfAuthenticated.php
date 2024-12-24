a6ad8b38c6dce775cca7f6650353d4f293bc8d08
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-12-24 20:20:01 +0600

    refactor: enhance authentication redirection logic to route users based on their roles

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,18 +10,21 @@ use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if (Auth::user()->type == 'admin') {
+                    return redirect()->route('admin.home');
+                } elseif (Auth::user()->type == 'manager') {
+                    return redirect()->route('manager.home');
+                } elseif (Auth::user()->type == 'staff') {
+                    return redirect()->route('staff.home');
+                } else {
+                    return redirect()->route('user.home');
+                }
             }
         }
 
