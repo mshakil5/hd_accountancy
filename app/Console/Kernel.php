7020d0b472af76d8e117274d44bf0609821855fd
Nazmus Sakib <nsakib.miraz@gmail.com>
2025-01-08 11:23:16 +0600

    feat: update service job scheduling to run every five minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('services:check-and-create')->everyMinute();
+        $schedule->command('services:check-and-create')->everyFiveMinutes();
     }
 
     /**
