f14dfd85a9bb0410d1fc3d967601b951198cd6c8
Nazmus Sakib <nsakib.miraz@gmail.com>
2025-02-10 11:12:45 +0600

    feat: integrate activity logging into ContactInfo model

--- app/Models/ContactInfo.php
+++ app/Models/ContactInfo.php
@@ -6,13 +6,26 @@ use App\Models\Client;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class ContactInfo extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes, LogsActivity;
     
     protected $guarded = [];
+
+    protected static $logAttributes = ['*'];
+    protected static $logOnlyDirty = true;
+    protected static $logName = 'contact_info';
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(array_diff(array_keys($this->getAttributes()), ['created_at', 'updated_at', 'deleted_at']))
+            ->useLogName('contact_info')
+            ->setDescriptionForEvent(fn(string $eventName) => "Contact Info record has been {$eventName}");
+    }
     
     public function client()
     {
