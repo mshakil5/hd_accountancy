6d71fbd1d49c5aff8a11de77d608b9305aa6fc19
Nazmus Sakib <nsakib.miraz@gmail.com>
2025-02-10 11:12:37 +0600

    feat: enhance RecentUpdate model with activity logging capabilities

--- app/Models/RecentUpdate.php
+++ app/Models/RecentUpdate.php
@@ -4,12 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class RecentUpdate extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
     
     protected $guarded = [];
+
+    protected static $logAttributes = ['*'];
+    protected static $logOnlyDirty = true;
+    protected static $logName = 'recent_update';
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(array_diff(array_keys($this->getAttributes()), ['created_at', 'updated_at']))
+            ->useLogName('recent_update')
+            ->setDescriptionForEvent(fn(string $eventName) => "Recent Update record has been {$eventName}");
+    }
 
     public function user()
     {
