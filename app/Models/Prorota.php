7eabc1104a7ffc019c01632a1574f35a2b1cadbc
Nazmus Sakib <nsakib.miraz@gmail.com>
2025-02-11 12:39:29 +0600

    feat: add activity logging to Prorota and ProrotaDetail models

--- app/Models/Prorota.php
+++ app/Models/Prorota.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Prorota extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    protected $guarded = [];
+
+    protected static $logAttributes = ['*'];
+    protected static $logOnlyDirty = true;
+    protected static $logName = 'prorota';
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(array_diff(array_keys($this->getAttributes()), ['created_at', 'updated_at', 'deleted_at']))
+            ->useLogName('prorota')
+            ->setDescriptionForEvent(fn(string $eventName) => "Prorota record has been {$eventName}");
+    }
 
     public function staff()
     {
