42b3d00e000bee120ee3c9faaba26613f0a80bca
Nazmus Sakib <nsakib.miraz@gmail.com>
2025-02-11 12:39:01 +0600

    feat: implement activity logging for ClientType model

--- app/Models/ClientType.php
+++ app/Models/ClientType.php
@@ -6,13 +6,26 @@ use App\Models\Client;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class ClientType extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes, LogsActivity;
 
     protected $guarded = [];
+
+    protected static $logAttributes = ['*'];
+    protected static $logOnlyDirty = true;
+    protected static $logName = 'client_type';
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(array_diff(array_keys($this->getAttributes()), ['created_at', 'updated_at', 'deleted_at']))
+            ->useLogName('client_type')
+            ->setDescriptionForEvent(fn(string $eventName) => "Client Type record has been {$eventName}");
+    }
 
     public function client ()
     {
