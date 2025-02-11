8a37d4c3e92642e6f9a565a82f56ed94f9ae4e8a
Nazmus Sakib <nsakib.miraz@gmail.com>
2025-02-11 12:39:21 +0600

    feat: add activity logging to HolidayRequest and HolidayType models

--- app/Models/HolidayRequest.php
+++ app/Models/HolidayRequest.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class HolidayRequest extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, LogsActivity;
 
     protected $guarded = [];
+
+    protected static $logAttributes = ['*'];
+    protected static $logOnlyDirty = true;
+    protected static $logName = 'holiday_request';
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(array_diff(array_keys($this->getAttributes()), ['created_at', 'updated_at', 'deleted_at']))
+            ->useLogName('holiday_request')
+            ->setDescriptionForEvent(fn(string $eventName) => "Holiday Request has been {$eventName}");
+    }
 
     public function staff()
     {
