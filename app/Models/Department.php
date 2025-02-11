2480f46d6ad304ba35c5951879668906bb934cbc
Nazmus Sakib <nsakib.miraz@gmail.com>
2025-02-11 12:39:10 +0600

    feat: add activity logging to Department model

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -6,13 +6,27 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Department extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes, LogsActivity;
 
     protected $guarded = [];
+
+    protected static $logAttributes = ['*'];
+    protected static $logOnlyDirty = true;
+    protected static $logName = 'department';
+
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(array_diff(array_keys($this->getAttributes()), ['created_at', 'updated_at', 'deleted_at']))
+            ->useLogName('department')
+            ->setDescriptionForEvent(fn(string $eventName) => "Department record has been {$eventName}");
+    }
 
     public function users()
     {
