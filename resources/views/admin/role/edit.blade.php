cb395072f66d7765ce7e807940f8c9a6c96ba384
Nazmus Sakib <nsakib.miraz@gmail.com>
2025-02-24 16:50:51 +0600

    feat: add toggle switch styling for role management views

--- resources/views/admin/role/edit.blade.php
+++ resources/views/admin/role/edit.blade.php
@@ -1,6 +1,54 @@
 @extends('admin.layouts.admin')
 
 @section('content')
+
+<style>
+  .switch {
+      position: relative;
+      display: inline-block;
+      width: 50px;
+      height: 25px;
+  }
+
+  .switch input {
+      opacity: 0;
+      width: 0;
+      height: 0;
+  }
+
+  .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      transition: .4s;
+      border-radius: 25px;
+  }
+
+  .slider:before {
+      position: absolute;
+      content: "";
+      height: 18px;
+      width: 18px;
+      left: 4px;
+      bottom: 3.5px;
+      background-color: white;
+      transition: .4s;
+      border-radius: 50%;
+  }
+
+  input:checked + .slider {
+      background-color: #233969;
+  }
+
+  input:checked + .slider:before {
+      transform: translateX(24px);
+  }
+</style>
+
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 
 <div class="row">
@@ -25,7 +73,7 @@
             <div class="col-md-10">
               <table class="table table-hover">
                 <tr>
-                  <td><label class="control-label">Role Name <span class="text-danger">*</span></label></td>
+                  <td><label class="control-label pt-2">Role Name <span class="text-danger">*</span></label></td>
                   <td>
                     <input name="name" id="name" type="text" class="form-control" maxlength="50px" value="{{ $data->name }}" required="required" />
                     <input name="id" id="id" type="hidden" class="form-control" maxlength="50px" value="{{ $data->id }}" required="required" />
