56d0fe270282dc4a1ff10c4c72484a5b4d308420
Sakib <nsakib.miraz@gmail.com>
2026-01-11 15:23:20 +0600

    issue fix

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -862,81 +862,70 @@ class ServiceController extends Controller
     public function getAssignedService(Request $request)
     {
         if ($request->ajax()) {
+            try {
+                $today = Carbon::today()->format('d-m-Y');
 
-            $today = Carbon::today()->format('d-m-Y');
+                $data = ClientService::whereNotNull('service_deadline')
+                    ->whereNotNull('due_date')
+                    ->whereNotNull('legal_deadline')
+                    ->whereNotNull('service_frequency')
+                    ->where('is_admin_approved', 0)
+                    ->where('type', 1)
+                    ->whereRaw("STR_TO_DATE(due_date, '%d-%m-%Y') <= STR_TO_DATE(?, '%d-%m-%Y')", [$today])
+                    ->whereHas('clientSubServices', function ($query) {
+                        $query->whereNotNull('staff_id');
+                    })
+                    ->orderByRaw("STR_TO_DATE(due_date, '%d-%m-%Y') ASC")
+                    ->get();
 
-            $data = ClientService::with('clientSubServices')
-                // ->whereIn('status', [0, 1, 2])
-                ->whereNotNull('service_deadline')
-                ->whereNotNull('due_date')
-                ->whereNotNull('legal_deadline')
-                ->whereNotNull('service_frequency')
-                ->where('is_admin_approved', 0)
-                ->where('type', 1)
-                ->whereRaw("STR_TO_DATE(due_date, '%d-%m-%Y') <= STR_TO_DATE(?, '%d-%m-%Y')", [$today])
-                ->whereHas('clientSubServices', function ($query) {
-                    $query->whereNotNull('staff_id');
-                })
-                ->orderByRaw("STR_TO_DATE(due_date, '%d-%m-%Y') ASC")
-                ->get();
-
-            return DataTables::of($data)
-
-                // ->addColumn('clientname', function (ClientService $clientservice) {
-                //     return $clientservice->client ? $clientservice->client->name : '';
-                // })
-                ->addColumn('clientname', function (ClientService $clientservice) {
-                    if ($clientservice->director_info_id) {
-                        return $clientservice->directorInfo->name;
-                    }
-                    return $clientservice->client->name;
-                })
-                ->addColumn('servicename', function (ClientService $clientservice) {
-                    return $clientservice->service ? $clientservice->service->name : '';
-                })
-                ->addColumn('legal_deadline', function (ClientService $clientservice) {
-                    $legalDeadline = $clientservice->legal_deadline;
-                    if ($legalDeadline) {
-                        return [
-                            'formatted' => \Carbon\Carbon::parse($legalDeadline)->format('d-m-Y'),
-                            'original' => $legalDeadline
-                        ];
-                    }
-
-                    return [
-                        'formatted' => 'N/A',
-                        'original' => null
-                    ];
-                })
-
-                ->addColumn('due_date', function (ClientService $clientservice) {
-                    $dueDate = $clientservice->due_date;
-                    if ($dueDate) {
-                        return \Carbon\Carbon::parse($dueDate)->format('d-m-Y');
-                    }
-                    return 'N/A';
-                })
-
-                ->addColumn('service_deadline', function (ClientService $clientservice) {
-                    $serviceDeadline = $clientservice->service_deadline;
-
-                    if ($serviceDeadline) {
-                        return [
-                            'formatted' => \Carbon\Carbon::parse($serviceDeadline)->format('d-m-Y'),
-                            'original' => $serviceDeadline
-                        ];
-                    }
-                    return [
-                        'formatted' => 'N/A',
-                        'original' => null
-                    ];
-                })
-                ->addColumn('action', function (ClientService $clientservice) {
-                    $managerFirstName = $clientservice->manager ? $clientservice->manager->first_name . ' ' . $clientservice->manager->last_name : 'N/A';
-                    return '<button class="btn btn-secondary task-detail" data-id="' . $clientservice->id . '" data-manager-firstname="' . $managerFirstName . '">Details</button>';
-                })
-                ->addIndexColumn()
-                ->make(true);
+                return DataTables::of($data)
+                    ->addColumn('clientname', function (ClientService $clientservice) {
+                        return $clientservice->director_info_id ? $clientservice->directorInfo?->name : $clientservice->client?->name;
+                    })
+                    ->addColumn('servicename', function (ClientService $clientservice) {
+                        return $clientservice->service?->name ?? '';
+                    })
+                    ->addColumn('service_frequency', function (ClientService $clientservice) {
+                        return $clientservice->service_frequency ?? '';
+                    })
+                    ->addColumn('legal_deadline', function (ClientService $clientservice) {
+                        $legalDeadline = $clientservice->legal_deadline;
+                        if ($legalDeadline) {
+                            return [
+                                'formatted' => \Carbon\Carbon::parse($legalDeadline)->format('d-m-Y'),
+                                'original' => $legalDeadline
+                            ];
+                        }
+                        return ['formatted' => 'N/A', 'original' => null];
+                    })
+                    ->addColumn('due_date', function (ClientService $clientservice) {
+                        return $clientservice->due_date ?? 'N/A';
+                    })
+                    ->addColumn('service_deadline', function (ClientService $clientservice) {
+                        $serviceDeadline = $clientservice->service_deadline;
+                        if ($serviceDeadline) {
+                            return [
+                                'formatted' => \Carbon\Carbon::parse($serviceDeadline)->format('d-m-Y'),
+                                'original' => $serviceDeadline
+                            ];
+                        }
+                        return ['formatted' => 'N/A', 'original' => null];
+                    })
+                    ->addColumn('is_admin_approved', function (ClientService $clientservice) {
+                        return $clientservice->is_admin_approved;
+                    })
+                    ->addColumn('action', function (ClientService $clientservice) {
+                        $managerFirstName = $clientservice->manager?->first_name . ' ' . $clientservice->manager?->last_name ?? 'N/A';
+                        return '<button class="btn btn-secondary task" data-id="' . $clientservice->id . '" data-manager-firstname="' . $managerFirstName . '">Details</button>';
+                    })
+                    ->addIndexColumn()
+                    ->make(true);
+            } catch (\Exception $e) {
+                return response()->json([
+                    'message' => $e->getMessage(),
+                    'line' => $e->getLine()
+                ], 500);
+            }
         }
     }
 
@@ -1041,7 +1030,6 @@ class ServiceController extends Controller
         if ($request->ajax()) {
             $today = Carbon::today()->format('d-m-Y');
             $data = ClientService::where('type', 1)
-                ->with('clientSubServices')
                 ->where('service_deadline', $today)
                 ->whereHas('clientSubServices', function ($query) {
                     $query->whereNotNull('staff_id');
@@ -1050,18 +1038,20 @@ class ServiceController extends Controller
                 ->get();
 
             return DataTables::of($data)
-
                 ->addColumn('clientname', function (ClientService $clientservice) {
-                    if ($clientservice->director_info_id) {
-                        return $clientservice->directorInfo->name;
-                    }
-                    return $clientservice->client->name;
+                    return $clientservice->director_info_id ? $clientservice->directorInfo?->name : $clientservice->client?->name;
                 })
                 ->addColumn('servicename', function (ClientService $clientservice) {
-                    return $clientservice->service->name;
+                    return $clientservice->service?->name ?? '';
+                })
+                ->addColumn('service_frequency', function (ClientService $clientservice) {
+                    return $clientservice->service_frequency ?? '';
+                })
+                ->addColumn('service_deadline', function (ClientService $clientservice) {
+                    return $clientservice->service_deadline ?? '';
                 })
                 ->addColumn('action', function (ClientService $clientservice) {
-                    $managerFirstName = $clientservice->manager ? $clientservice->manager->first_name . ' ' . $clientservice->manager->last_name : 'N/A';
+                    $managerFirstName = $clientservice->manager?->first_name . ' ' . $clientservice->manager?->last_name ?? 'N/A';
                     return '<button class="btn btn-secondary task" data-id="' . $clientservice->id . '" data-manager-firstname="' . $managerFirstName . '">Details</button>';
                 })
                 ->make(true);
