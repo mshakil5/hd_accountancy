11252127dbaa0dfa044407104a9fa5215fdd7bf0
Sakib <nsakib.miraz@gmail.com>
2026-09-16 23:53:08 +0600

    Remove DualAuth middleware and update AuthServiceProvider to handle user resolution more effectively

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,31 +2,33 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
+use Laravel\Passport\Token;
+use App\Models\User;
+use App\Models\ClientCredential;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
         $this->registerPolicies();
+
+        Passport::resolveUserUsing(function ($tokenId) {
+            $token = Token::find($tokenId);
+            if (!$token) return null;
+
+            $user = User::find($token->user_id);
+            if ($user) return $user;
+
+            return ClientCredential::find($token->user_id);
+        });
     }
 
     public function register()
     {
-      // Passport::ignoreRoutes();
     }
 }
